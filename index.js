cb12710c073f5765be2a457723c9cdf8ecd5af64
ToBeginToPonderOver <121504896+ToBeginToPonderOver@users.noreply.github.com>
2023-08-20 20:36:42 +0530

    some changes

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ['CSE Student', 'Frontend Web-Developer' , 'UX / UI Learner'],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+  });
